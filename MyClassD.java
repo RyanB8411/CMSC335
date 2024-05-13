c42aea0b9cdc26a007623cf16ca2617ccd3e0731
Ryan <RyanABurkhardt@gmail.com>
2024-05-13 19:48:06 -0400

    Working Homework1

---
+++ MyClassD.java
@@ -0,0 +1,15 @@
+public class MyClassD { 
+    public static void main (String args []) { 
+        MyClassC m = new MyClassC (23);
+        System.out.println(m);
+    } // end main 
+} // end class MyClassD 
+class MyClassC { 
+    int v = 12; 
+    public MyClassC (int pV) { 
+        v = pV;
+    }public String toString(){
+        return "v = " + v;
+    }
+} // end class MyClassC 
+    
