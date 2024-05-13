45a0a7b6c740cf6dc4d20be42cbfdc5eee494e3b
Ryan <RyanABurkhardt@gmail.com>
2024-05-13 11:09:16 -0400

    Working

--- MyClassA.java
+++ MyClassA.java
@@ -1,1 +1,13 @@
-
+public class MyClassA {
+    int v = 12; 
+    public MyClassA (int pV) { 
+        v = pV;
+    }
+    public String toString(){
+        return "v = " + v;
+    }
+    public static void main (String args []) { 
+        MyClassA m = new MyClassA (22); 
+        System.out.println(m);
+    } // end main 
+} // end class MyClassA 
